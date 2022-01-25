a2e5b1e27e72b34ee036f63f54c2b4eb7ca5f62e
Dipesh Mitthalal <dipesh.mnm@gmail.com>
2022-01-25 16:56:36 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
     String awskey="AKIAIOSFODNN7EXAMPLE";
     String secretAccesskey="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
     String SPECTRAL_DSN = "https://spk-ea12936d38e14a7988fe4a9932abcdef@get.spectralops.io"
+    String aws_access_key = "AKIA6ALNLGE7BUGVY7OA";
+    String aws_secret = "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";
     System.out.println("Hello, World");
   }
 }
