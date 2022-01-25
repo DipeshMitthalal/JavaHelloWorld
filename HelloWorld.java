6c15de162909591bcf8d42ce8a932d5c63f81a37
Dipesh Mitthalal <dipesh.mnm@gmail.com>
2022-01-25 15:49:41 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    String password="updatedpassword2";
+    String passwd="updatedpassword4";
+    String awskey="AKIAIOSFODNN7EXAMPLE";
+    String secretAccesskey="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+    String SPECTRAL_DSN = "https://spk-ea12936d38e14a7988fe4a9932abcdef@get.spectralops.io"
+    System.out.println("Hello, World");
+  }
+}
