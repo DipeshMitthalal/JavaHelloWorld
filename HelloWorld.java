8853700c95ae0b1c09f5028a7c81fde12234ff4b
Dipesh Mitthalal <dipesh.mnm@gmail.com>
2022-01-26 15:29:28 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,10 @@
-public class HelloWorld {
+public class Helloworld {
   public static void main(String[] args) {
-    String password="updatedpassword2";
-    String passwd="updatedpassword4";
-    String awskey="AKIAIOSFODNN7EXAMPLE";
-    String secretAccesskey="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-    String SPECTRAL_DSN = "https://spk-ea12936d38e14a7988fe4a9932abcdef@get.spectralops.io"
-    String aws_access_key = "AKIA6ALNLGE7BUGVY7OA";
-    String aws_secret = "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";
-    System.out.println("Hello, World");
+  Greeter greeter = new Greeter();
+  String password="updatedpassword2";
+  String passwd="updatedpassword4";
+  String awskey="AKIA6ALNLGE7BUGVYABC";
+  String secretAccesskey="p03p0uO+iHOqKDW10V6UQa8Dhk0zmiSYZpG8GRE";
+  String SPECTRAL_DSN = "https://spk-ea12936d38e14a7988fe4a9932fae239@get.spectralops.io";
   }
 }
